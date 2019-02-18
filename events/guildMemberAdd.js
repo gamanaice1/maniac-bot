d363a11f15a04fa600fc08e364805d22b8baa85a
gamanaice1 <47690847+gamanaice1@users.noreply.github.com>
2019-02-18 23:53:29 +0300

    node_modules

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('https://media.giphy.com/media/XKGCVp7BOAkISkfSbj/giphy.gif Selamlar,Değerli **' + username + '** Discord Sunucuma Hoşgeldin :kissing_heart: #kurallar a bakmayı unutma,lazım olacağından eminim. :thinking: Keyifli Vakit Geçirmen Dileğiyle :wink:');
+
+};
---
+++ komutlar/bilgi.js
@@ -0,0 +1,32 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message) => {
+  if (message.channel.type !== 'dm') {
+    const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Özel mesajlarını kontrol et. :postbox:');
+    message.channel.sendEmbed(ozelmesajkontrol) }
+	const pingozel = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Bot sürümü: v' + ayarlar.surum + ' Yapımcı: GAM YAZILIM  **xEterNally&GAManiacE**\n\n_**BOTU EKLEMEK İÇİN LİNK:**_\n\nhttps://discordapp.com/oauth2/authorize?client_id=546362677824585738&scope=bot&permissions=805829694 \n_**Linkler:**_\n\n**GAM YAZILIM** sunucusunun davet linki: https://discord.gg/WzJhAgD \n\n**:copyright: 2019 [GAM] Yazılım**');
+    return message.author.sendEmbed(pingozel)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['bot bilgi', 'botbilgi', 'bb', 'botb', 'bbot', 'hakkında', 'bot hakkında', 'bothakkında'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'bilgi',
+  description: 'Bot ile ilgili bilgi verir.',
+  usage: 'bilgi'
+};
